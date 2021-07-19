913b2bf2a940d1d527584f19dc97e8b4dee331bd
kevinchewning <klcbusiness@hotmail.com>
2021-07-19 13:55:34 -0400

    fixed directories

---
+++ Assets/scripts/script.js
@@ -0,0 +1,13 @@
+//Hamburger menu function
+$(document).ready(function() {
+
+    // Check for click events on the navbar burger icon
+    $(".navbar-burger").click(function() {
+  
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        $(".navbar-burger").toggleClass("is-active");
+        $(".navbar-menu").toggleClass("is-active");
+        $(".navbar-divider").toggleClass("display-block");
+  
+    });
+});
